de35ca53c8e52e47ff173c06fd7bb9149a53f522
Michal Rowinski <121058727+Meefio@users.noreply.github.com>
2024-10-12 23:37:36 +0200

    add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-
+import Hero from "@/components/Hero"
 
 
 export default function Home() {
   return (
     <main className='wrapper mx-auto py-8 h-screen'>
-
+      <Hero />
     </main>
   )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+import hero from '../../public/hero-image.png'
+
+const Hero = () => {
+	return (
+		<section>
+			<div className='mt-24'>
+				<h1 className=''>
+					Profesjonalne mycie <br />{' '}
+					<span className='text-main-color'>kostki brukowej</span>
+				</h1>
+			</div>
+			<Image
+				src={hero}
+				width={1080}
+				height={949}
+				alt='Pracownik myjący elewację'
+				className='max-w-[30vh] mx-auto my-6'
+			/>
+			<p className='max-w-[70%] mx-auto text-xl text-center'>
+				Kompleksowe czyszczenie i impregnacja powierzchni. Sprawdź,
+				jak przywrócimy blask Twojego otoczenia!
+			</p>
+		</section>
+	)
+}
+
+export default Hero
